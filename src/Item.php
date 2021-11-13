e062b2e66f48b3c9a4a73ffd7ca228a09cf8cc76
Diego F. Trejos Espinosa <dyfer92@gmail.com>
2021-11-13 16:49:45 +0100

    feat: add php-cs-fixer and fix src and test files

--- src/Item.php
+++ src/Item.php
@@ -2,27 +2,29 @@
 
 namespace Runroom\GildedRose;
 
-class Item {
-    const FIXED_NAMES = [
+class Item
+{
+    public const FIXED_NAMES = [
         'AGED'      => 'Aged Brie',
         'BACKSTAGE' => 'Backstage passes to a TAFKAL80ETC concert',
         'SULFURA'   => 'Sulfuras, Hand of Ragnaros',
     ];
 
-    const MAX_QUALITY = 50;
+    public const MAX_QUALITY = 50;
 
     public string $name;
     public int $sell_in;
     public int $quality;
 
-    function __construct(string $name, int $sell_in, int $quality) {
+    public function __construct(string $name, int $sell_in, int $quality)
+    {
         $this->name = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return "{$this->name}, {$this->sell_in}, {$this->quality}";
     }
-
 }
