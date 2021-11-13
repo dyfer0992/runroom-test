0fb1cf6aafebf3b92740ebe6bc4b535179c8f95d
Diego F. Trejos Espinosa <dyfer92@gmail.com>
2021-11-13 15:47:20 +0100

    feat: Aged and Back item increases quality if is less than 50

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -20,8 +20,6 @@ class GildedRose {
             $this->updateItemQuality($item);
 
             if ($item->name == Item::FIXED_NAMES['AGED'] || $item->name == Item::FIXED_NAMES['BACKSTAGE']) {
-                if ($item->quality < Item::MAX_QUALITY) {
-                    $item->quality = $item->quality + 1;
                     if ($item->name == Item::FIXED_NAMES['BACKSTAGE']) {
                         if ($item->sell_in < 11) {
                             if ($item->quality < Item::MAX_QUALITY) {
@@ -34,7 +32,6 @@ class GildedRose {
                             }
                         }
                     }
-                }
             }
 
             $item->sell_in = $item->sell_in - 1;
@@ -43,14 +40,8 @@ class GildedRose {
 
                 $this->updateItemQuality($item);
 
-                if ($item->name != Item::FIXED_NAMES['AGED']) {
-                    if ($item->name == Item::FIXED_NAMES['BACKSTAGE']) {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < Item::MAX_QUALITY) {
-                        $item->quality = $item->quality + 1;
-                    }
+                if ($item->name == Item::FIXED_NAMES['BACKSTAGE']) {
+                    $item->quality = $item->quality - $item->quality;
                 }
             }
         }
@@ -64,6 +55,11 @@ class GildedRose {
             && $item->quality > 0
         ) {
             $item->quality--;
+            return;
+        }
+
+        if ($item->quality < Item::MAX_QUALITY) {
+            $item->quality++;
         }
     }
 }
