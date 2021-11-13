9e9fbbba8dce50d7836764fb98158434471a2925
Diego F. Trejos Espinosa <dyfer92@gmail.com>
2021-11-13 16:09:42 +0100

    feat: backstage logic
    - if sell_in is negative, quality is always zero
    - increase 2 if sell_in is between 0 and 5
    - increase 1 if sell_in is between 6 and 10

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -19,30 +19,10 @@ class GildedRose {
 
             $this->updateItemQuality($item);
 
-            if ($item->name == Item::FIXED_NAMES['AGED'] || $item->name == Item::FIXED_NAMES['BACKSTAGE']) {
-                    if ($item->name == Item::FIXED_NAMES['BACKSTAGE']) {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < Item::MAX_QUALITY) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < Item::MAX_QUALITY) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-            }
-
-            $item->sell_in = $item->sell_in - 1;
+            $item->sell_in--;
 
             if ($item->sell_in < 0) {
-
                 $this->updateItemQuality($item);
-
-                if ($item->name == Item::FIXED_NAMES['BACKSTAGE']) {
-                    $item->quality = $item->quality - $item->quality;
-                }
             }
         }
     }
@@ -60,6 +40,18 @@ class GildedRose {
 
         if ($item->quality < Item::MAX_QUALITY) {
             $item->quality++;
+        }
+
+        if ($item->name == Item::FIXED_NAMES['BACKSTAGE']) {
+            if ($item->sell_in < 0) {
+                $item->quality -= $item->quality;
+                return;
+            }
+            if ($item->sell_in < 6) {
+                $item->quality += 2;
+            } elseif ($item->sell_in < 11) {
+                $item->quality++;
+            }
         }
     }
 }
